48e6796bf8c72dc9af576d3d8aeb6a3973ab83ea
Maria <mcavanaghnicholson@udesa.edu.ar>
2025-09-16 17:07:28 -0300

    screens

---
+++ src/screens/verCartelera/verCartelera.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PeliculasCartel from "../../components/PeliculasCartel/PeliculasCartel";
+
+class verCartelera extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            peliculas: [],
+
+        }
+    }
+}
+
