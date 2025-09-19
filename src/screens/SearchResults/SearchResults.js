1377ac2438789a3a41382a67e8d39ae87fc79d53
Maria <mcavanaghnicholson@udesa.edu.ar>
2025-09-19 19:08:40 -0300

    buscador y css

--- src/screens/SearchResults/SearchResults.js
+++ src/screens/SearchResults/SearchResults.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import PeliculaCard from "../../components/PeliculaCard/PeliculaCard";
+import "./SearchResults.css"; 
 
 class SearchResults extends Component {
     constructor(props) {
@@ -26,18 +27,33 @@ class SearchResults extends Component {
         
     }
 
+    componentDidUpdate(prevProps){
+        if (prevProps.location.search !== this.props.location.search) {
+            const queryParams = new URLSearchParams(this.props.location.search);
+            const type = queryParams.get("type");  
+            const query = queryParams.get("query"); 
+    
+            this.setState({ busqueda: query });
+    
+            fetch(`https://api.themoviedb.org/3/search/${type}?api_key=e869b9e987b5106b290be42193522eba&language=en-US&query=${query}`)
+            .then(res => res.json())
+            .then(data => this.setState({resultados: data.results}))
+            .catch(error => console.error(error));
+        }
+    }
+
     render(){
         return (
             <React.Fragment>
                 <Navbar/>
                 <h2 className="resul"> Resultados de: {this.state.busqueda}</h2>
-                <div>
+                <section className="cuadradosresul">
                     {this.state.resultados.length > 0
                         ?
                         (this.state.resultados.map(elm => (
                             <PeliculaCard  
                             key={elm.id}
-                            nombre={elm.title}   
+                            nombre={elm.title} 
                             imagen={elm.poster_path}
                             descripcion={elm.overview}
                             id={elm.id}/>
@@ -47,7 +63,7 @@ class SearchResults extends Component {
                         <h2>No hay resultados disponibles</h2>
 
                     )}
-                </div>
+                </section>
                 <Footer/>
 
             </React.Fragment>
